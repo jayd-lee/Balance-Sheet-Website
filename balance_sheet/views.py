ce46cf1937425dc9833148201676b48e287947cf
Jayden Lee <jaydeh.lee@gmail.com>
2023-04-03 18:51:55 -0400

    Set up only the front end home page. No backend

--- balance_sheet/views.py
+++ balance_sheet/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 def home_view(request):
 
-    context = {}
+    context = {
+        
+    }
 
     return render(request, 'home-view.html', context=context)
 
